5ea71287e850a77abfd2a546ac2bf58246070c5e
pelle <pelle.hammarstromberglof@elev.ga.ntig.se>
2023-02-06 16:36:20 +0100

    lektion 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-I denna mapp placeras js-dokument.
+const burger = document.getElementById('burger')
+const nav = document.getElementById('nav-links')
+
+function toggleMenu() {
+    burger.classList.toggle('burger_toggle')
+    nav.classList.toggle('nav_active')
+    console.log("hej")
+}
+
+burger.addEventListener('click', toggleMenu)
