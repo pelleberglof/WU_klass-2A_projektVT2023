aa80c8d553f6fc299cdf5b6ff63016dd1b8f9a54
pelle <pelle.hammarstromberglof@elev.ga.ntig.se>
2023-04-03 15:55:30 +0200

    färdigt projekt

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const nav = document.getElementById('nav-links')
 function toggleMenu() {
     burger.classList.toggle('burger_toggle')
     nav.classList.toggle('nav_active')
-    console.log("hej")
 }
 
 burger.addEventListener('click', toggleMenu)
